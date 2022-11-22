66a6f84b4d44abaec4c9b8c8a77ba237e3daedc2
john.arango <johnj42@gmail.com>
2022-11-21 23:54:39 -0500

    Map Screen indentation and JavaDocs adjusted

--- src/test/java/screens/MapScreen.java
+++ src/test/java/screens/MapScreen.java
@@ -3,11 +3,8 @@ package screens;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.HowToUseLocators;
 import util.screens.BaseScreen;
 import java.util.List;
-
-import static io.appium.java_client.pagefactory.LocatorGroupStrategy.ALL_POSSIBLE;
 
 /**
  * Map screen.
@@ -33,16 +30,6 @@ public class MapScreen extends BaseScreen {
 
     @AndroidFindBy(uiAutomator = "UiSelector().resourceIdMatches(\".*facilityTypeTitle\").text(\"Hotels\")")
     private AndroidElement hotelsIncluded;
-
-    @HowToUseLocators(androidAutomation = ALL_POSSIBLE)
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*filterTitle.*\")")
-    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Filter\")")
-    private AndroidElement filterButton;
-
-    @HowToUseLocators(androidAutomation = ALL_POSSIBLE)
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*toggleTitle.*\")")
-    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Show List\")")
-    private AndroidElement showListButton;
 
     /**
      * @author Hans.Marquez
