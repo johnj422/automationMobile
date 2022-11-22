20bab4b99fb577cb4c1bff2c2f400a2d4c19c47b
john.arango <johnj42@gmail.com>
2022-11-22 00:33:21 -0500

    Listener included and set

--- src/test/java/util/Listener.java
+++ src/test/java/util/Listener.java
@@ -1,16 +1,20 @@
 package util;
 
-
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.tinylog.Logger;
 
-
-public abstract class Listener implements ITestListener {
+public class Listener implements ITestListener {
 
     public static final String PassedColor = "\u001B[32m";
     public static final String FailedColor =  "\u001B[31m";
     public static final String ResetColor = "\u001B[0m";
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
 
     @Override
     public void onTestSuccess(ITestResult result) {
@@ -20,5 +24,25 @@ public abstract class Listener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         Logger.error("Test " + result.getName() + "..." + FailedColor + "[FAILED]" + ResetColor);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
     }
 }
